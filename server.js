1ea3ff83ed8c5e15ab0a0a6b31c633d90ca89de7
h-yung <helen.yung.h@gmail.com>
2022-07-12 13:50:40 -0400

    working tag arrays in create and update

--- server.js
+++ server.js
@@ -70,8 +70,9 @@ MongoClient.connect(process.env.DATABASE_URL, {
                     })
                     .catch(error => console.error(error))
                 }else if (req.body.field === 'Tags'){
+                    const newTags = req.body.edit.split(', ')
                     thingGroup.updateOne({title: req.body.title}, {
-                        $addToSet: { 'tags': req.body.edit }
+                        $addToSet: { 'tags': [...newTags] }
                     },{
                         sort: {_id: -1}, //not sure this does anything for me - is it listing from most recent entry to oldest?
                         upsert: false //don't add if doesn't exist based on title spec
